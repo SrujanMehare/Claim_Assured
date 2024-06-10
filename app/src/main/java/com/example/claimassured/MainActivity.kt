3b2b96426f7dcd5e39463b74b26fba00f2410464
SrujanMehre <meharesrujan@gmail.com>
2024-06-10 23:55:37 +0530

    1. Added Bottom Navigation view

--- app/src/main/java/com/example/claimassured/MainActivity.kt
+++ app/src/main/java/com/example/claimassured/MainActivity.kt
@@ -7,12 +7,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import com.example.claimassured.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -35,6 +40,8 @@ class MainActivity : AppCompatActivity() {
 //                hideStatusBar()
 //            }
 //        }
+        binding.bottomNavigationView.background = null
+        binding.bottomNavigationView.menu.getItem(2).isEnabled = false
     }
 
     //** Hide status bar **//
